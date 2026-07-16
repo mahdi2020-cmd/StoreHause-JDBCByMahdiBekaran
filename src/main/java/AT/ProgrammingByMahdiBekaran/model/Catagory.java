f72e05c5feac5563b5c0a0665cbe68448818bce9
mahdi <Mahdibekaran123@gmail.com>
2026-07-16 19:26:33 +0200

    create catagory by id and name

--- src/main/java/AT/ProgrammingByMahdiBekaran/model/Catagory.java
+++ src/main/java/AT/ProgrammingByMahdiBekaran/model/Catagory.java
@@ -1,9 +1,18 @@
 package AT.ProgrammingByMahdiBekaran.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Catagory {
     private int id;
     private String name;
+
+    public Catagory(String name) {
+        this.name = name ;
+    }
 }
