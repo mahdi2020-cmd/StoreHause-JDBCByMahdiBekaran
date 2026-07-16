a2d8727386fd682fb4fbae49fa9f2f26b38bf2be
mahdi <Mahdibekaran123@gmail.com>
2026-07-16 19:27:44 +0200

    manage error

--- src/main/java/AT/ProgrammingByMahdiBekaran/utilitie/JDbcConnection.java
+++ src/main/java/AT/ProgrammingByMahdiBekaran/utilitie/JDbcConnection.java
@@ -2,10 +2,11 @@ package AT.ProgrammingByMahdiBekaran.utilitie;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class JDbcConnection {
-    private static final String url = "jdbc:postgresql://localhost:5432/storHaus";
+    private static final String url = "jdbc:postgresql://localhost:5432/storhous";
     private static final String username= "postgres";
     private static final String password = "21212";
 
@@ -15,5 +16,15 @@ public class JDbcConnection {
         final Connection connection = DriverManager.getConnection(url, username, password);
         System.out.println("to database connented :) ");
         return connection;
+    }
+    public static void closeResources(Connection con, PreparedStatement ps) {
+        try {
+            if (ps != null)
+                ps.close();
+            if (con != null)
+                con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
