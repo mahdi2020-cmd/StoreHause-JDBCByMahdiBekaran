64e233fd7694f362d63901bcd1e78d9363b305e3
mahdi <Mahdibekaran123@gmail.com>
2026-07-16 19:27:25 +0200

    create method and can call the connection and check uniqe name

--- src/main/java/AT/ProgrammingByMahdiBekaran/repository/CatagoryRepository.java
+++ src/main/java/AT/ProgrammingByMahdiBekaran/repository/CatagoryRepository.java
@@ -1,4 +1,45 @@
 package AT.ProgrammingByMahdiBekaran.repository;
 
+import AT.ProgrammingByMahdiBekaran.model.Catagory;
+import AT.ProgrammingByMahdiBekaran.utilitie.JDbcConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CatagoryRepository {
+
+    Connection connection = JDbcConnection.connection();
+
+    public CatagoryRepository() throws SQLException {
+    }
+
+    public int save(String catagory) throws SQLException {
+
+        String query = "INSERT INTO storhaus(name) VALUES (?)";
+        final PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, catagory);
+        final int i = preparedStatement.executeUpdate();
+        //JDbcConnection.closeResources(connection, preparedStatement);
+
+        preparedStatement.close();
+        connection.close();
+        return i;
+    }
+    public boolean uniqeName(String catagory) throws SQLException {
+        String query = "SELECT * FROM storhaus s WHERE s.name = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, catagory);
+        final ResultSet resultSet = preparedStatement.executeQuery();
+        boolean find = resultSet.next();
+//        resultSet.close();
+//        preparedStatement.close();
+//        connection.close();
+        System.out.println(find);
+
+        return find;
+
+
+    }
 }
